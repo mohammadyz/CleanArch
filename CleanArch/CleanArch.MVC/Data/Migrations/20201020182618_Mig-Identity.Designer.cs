b5ad13751a787ef06439546faabb888884e609e7
Asus <Asus@M-AMIN>
2020-10-20 22:18:03 +0330

    Config Class Libraries

--- CleanArch/CleanArch.MVC/Data/Migrations/20201020182618_Mig-Identity.Designer.cs
+++ CleanArch/CleanArch.MVC/Data/Migrations/20201020182618_Mig-Identity.Designer.cs
@@ -4,14 +4,16 @@ using CleanArch.MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArch.MVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201020182618_Mig-Identity")]
+    partial class MigIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CleanArch/CleanArch.MVC/Startup.cs
+++ CleanArch/CleanArch.MVC/Startup.cs
@@ -29,11 +29,16 @@ namespace CleanArch.MVC
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("CleanArchIdentityDb")));
+           
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+
+           // services.AddDbContext<>()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
