2ffe00cbc80cae3cac8b8a16d4d3b8b23c1c44fb
Asus <Asus@M-AMIN>
2020-10-20 22:36:43 +0330

    Add CourseRepositores

---
+++ CleanArch/CleanArch.Domain/Interfaces/ICourseRepositores.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Model;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICourseRepositores
+    {
+        IEnumerable<Course> GetCourse();
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repositores/CourseRepositores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Model;
+using CleanArch.Infra.Data.Context;
+
+namespace CleanArch.Infra.Data.Repositores
+{
+    public class CourseRepositores : ICourseRepositores
+    {
+        private CleanArchDbContext db;
+
+        public CourseRepositores(CleanArchDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IEnumerable<Course> GetCourse()
+        {
+            return db.Courses.ToList();
+        }
+    }
+}
